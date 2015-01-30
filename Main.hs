69f0b0778f6abaed117471f16db41374e69209e8
chrisdotcode <chris@code.sc>
2015-01-30 17:20:44 -0500

    Switches to 4-space indentation; adds -Wall.

--- Main.hs
+++ Main.hs
@@ -8,10 +8,10 @@ import Data.Attoparsec.ByteString
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-      (x:xs) -> B.readFile x >>= print . parseOnly xbase
-      _      -> error "Please supply a .dbf file as the first arg."
+    args <- getArgs
+    case args of
+        (x:xs) -> B.readFile x >>= print . parseOnly xbase
+        _      -> error "Please supply a .dbf file as the first arg."
 
 data DBF = DBF { version :: Integer } deriving (Show)
 
